bd9aaad7a9d021d28e5849b2250111711f9d4589
Gabriel Pinheiro <gabrielpsilvas@live.com>
2020-09-22 17:29:43 -0300

    Add RTL config and first test

---
+++ src/components/Main/test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react'
+
+import Main from '.'
+
+//Describe => Descrever algo que será testado.
+//Nesse caso iremos testar o Component <Main />
+describe('<Main />', () => {
+  //Escrevendo o teste de algo que esperamos que aconteça no Component
+  it('Should render the heading', () => {
+    const { container } = render(<Main />)
+
+    //Esperamos que encontre (aconteça) heading (h1,h2,h3...) escrito "react avançado"
+    expect(
+      screen.getByRole('heading', { name: /react avançado/i })
+    ).toBeInTheDocument() //E que o mesmo exista no Document
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
+
+//SnapShot
